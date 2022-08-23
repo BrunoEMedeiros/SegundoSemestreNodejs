50f631df0fd84d0650959850a34dfcd9f1a4da75
Prof Bruno M <bruno.emedeiros99@gmail.com>
2022-08-23 12:27:36 -0300

    23_08

--- src/Controllers/ClientController.js
+++ src/Controllers/ClientController.js
@@ -2,16 +2,36 @@ import { Client } from "../Models/ClientModel.js";
 
 export class ClientController{
 
-    static listarTodos( req, res){
-        const clientes = new Client().SelectAll()
-        return res.status(200).json(clientes)
+    static async listarTodos( req, res){
+        try {
+            const clientes = await Client.SelectAll();
+            return res.status(200).json(clientes);
+        } catch (error) {
+            console.log('Error controller ClientController.js: ' + error)
+            return res.status(500).json(error)
+        }
     }
 
-    static novoCliente(req, res){
-        const { nome , idade } = req.body;
-        const cliente = new Client(nome, idade).Insert();
-        return res.status(200).json({
-            msg : 'Cadastrado com sucesso!'
-        });
+    static async novoCliente(req, res){
+        try {
+            const { nome, idade, cpf } = req.body;
+            const novo_cliente = await new Client(nome, idade, cpf).Insert();
+            return res.status(200).json(novo_cliente);
+        } catch (error) {
+            console.log("error controller: " + error)
+            return res.status(500).json(error)
+        }
+    }
+
+    static async alterarCliente(req, res){
+        try {
+            const { nome, idade, cpf } = req.body;
+            const { id } = req.params;
+            const alt_cliente = await new Client(nome, idade, cpf, id).Update();
+            return res.status(200).json(alt_cliente);
+        } catch (error) {
+            console.log("error controller: " + error)
+            return res.status(500).json(error)
+        }
     }
 }
--- test_bd.js
+++ test_bd.js
@@ -3,22 +3,30 @@ import { sqlConfig } from './src/Utils/database.js'
 
 export async function teste_conexao(){
    
-    sql.on('error', err => {
-        console.log('lib teste_bd.js: ' + err);
-        return false;
-    })
-
-    const pool = await sql.connect(sqlConfig);
-
-    if (pool._connected == true)
+        sql.on('error', err => {
+            console.log('lib teste_bd.js: ' + err);
+            return false;
+        })
+    
+        const pool = await sql.connect(sqlConfig);
+    
+    try
     {
-        console.log('Conexao estabelecida!');
-        pool.close();
-        return true
+        if (pool._connected == true)
+        {
+            console.log('Conexao estabelecida!');
+            pool.close();
+            return true
+        }
+        else{
+            console.log('connection teste_bd.js: ' + pool);
+            pool.close();
+            return false;
+        }
     }
-    else{
-        console.log('connection teste_bd.js: ' + pool);
+    catch(error){
         pool.close();
+        console.log('error teste_bd.js: ' + error)
         return false;
     }
 
